8bcb3fcf7d1be5a8fae46ef0e0b0394751455cfc
Sam Bowden <srb55@student.le.ac.uk>
2022-03-24 15:35:23 +0000

    Add title screen and convert references from rocket to drone

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use heron::prelude::*;
 use bevy_prototype_lyon::prelude::*;
 
 #[derive(Component)]
-struct Rocket;
+struct Drone;
 
 #[derive(Component)]
 struct Camera;
@@ -15,26 +15,109 @@ struct SpawnTimer(Timer);
 
 struct CanonTimer(Timer);
 
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+enum AppState {
+    Menu,
+    InGame,
+}
+
+struct MenuData {
+    button_entity: Entity,
+}
+
+const NORMAL_BUTTON: Color = Color::rgb(0.15, 0.15, 0.15);
+const HOVERED_BUTTON: Color = Color::rgb(0.25, 0.25, 0.25);
+const PRESSED_BUTTON: Color = Color::rgb(0.35, 0.75, 0.35);
+
 fn main() {
     App::new()
         .insert_resource(Gravity::from(Vec3::new(0.0, -180., 0.0)))
         .insert_resource(ClearColor(Color::rgb(0., 0., 0.)))
         .insert_resource(SpawnTimer(Timer::from_seconds(0.1, false)))
         .insert_resource(CanonTimer(Timer::from_seconds(0.5, false)))
+        .add_state(AppState::Menu)
         .add_plugins(DefaultPlugins)
         .add_plugin(ShapePlugin)
         .add_plugin(PhysicsPlugin::default())
-        .add_startup_system(setup.system())
-        .add_startup_system(spawn_rocket.system())
-        .add_startup_system(spawn_floor.system())
-        .add_startup_system(position_text.system())
-        .add_system(update_position_text.system())
-        .add_system(rocket_movement.system())
-        .add_system(shoot_missiles.system())
+        .add_system_set(SystemSet::on_enter(AppState::Menu).with_system(setup_menu))
+        .add_system_set(SystemSet::on_update(AppState::Menu).with_system(menu))
+        .add_system_set(SystemSet::on_exit(AppState::Menu).with_system(cleanup_menu))
+        .add_system_set(
+            SystemSet::on_enter(AppState::InGame)
+            .with_system(setup_game)
+            .with_system(spawn_drone)
+            .with_system(spawn_floor)
+            .with_system(position_text),
+        )
+        .add_system_set(
+            SystemSet::on_update(AppState::InGame)
+                .with_system(update_position_text)
+                .with_system(drone_movement)
+                .with_system(shoot_missiles),
+        )
         .run();
 }
 
-fn setup(
+fn setup_menu(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn_bundle(UiCameraBundle::default());
+    let button_entity = commands
+        .spawn_bundle(ButtonBundle {
+            style: Style {
+                size: Size::new(Val::Px(250.0), Val::Px(65.0)),
+                margin: Rect::all(Val::Auto),
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::Center,
+                ..Default::default()
+            },
+            color: NORMAL_BUTTON.into(),
+            ..Default::default()
+        })
+        .with_children(|parent| {
+            parent.spawn_bundle(TextBundle {
+                text: Text::with_section(
+                    "Start Level 1",
+                    TextStyle {
+                        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                        font_size: 40.0,
+                        color: Color::rgb(0.9, 0.9, 0.9),
+                    },
+                    Default::default(),
+                ),
+                ..Default::default()
+            });
+        })
+        .id();
+    commands.insert_resource(MenuData { button_entity });
+}
+
+fn menu(
+    mut state: ResMut<State<AppState>>,
+    mut interaction_query: Query<
+        (&Interaction, &mut UiColor),
+        (Changed<Interaction>, With<Button>),
+    >,
+) {
+    for (interaction, mut color) in interaction_query.iter_mut() {
+        match *interaction {
+            Interaction::Clicked => {
+                *color = PRESSED_BUTTON.into();
+                state.set(AppState::InGame).unwrap();
+            }
+            Interaction::Hovered => {
+                *color = HOVERED_BUTTON.into();
+            }
+            Interaction::None => {
+                *color = NORMAL_BUTTON.into();
+            }
+        }
+    }
+}
+
+fn cleanup_menu(mut commands: Commands, menu_data: Res<MenuData>) {
+    commands.entity(menu_data.button_entity).despawn_recursive();
+}
+
+fn setup_game(
     mut commands: Commands,
 ) {
     commands.spawn().insert_bundle(OrthographicCameraBundle::new_2d()).insert(Camera);
@@ -82,7 +165,7 @@ fn position_text(
 
 fn update_position_text(
     mut q: Query<&mut Text, With<PositionText>>,
-    x: Query<&Transform, With<Rocket>>,
+    x: Query<&Transform, With<Drone>>,
 ) {
     for mut text in q.iter_mut() {
         for transform in x.iter() {
@@ -100,11 +183,11 @@ fn shoot_missiles(
     mut commands: Commands,
     mut timer: ResMut<SpawnTimer>,
     time: Res<Time>,
-    rocket: Query<&Transform, With<Rocket>>,
+    drone: Query<&Transform, With<Drone>>,
 ) {
     timer.0.tick(time.delta());
     if timer.0.finished() {
-        if input.pressed(KeyCode::Q) || input.pressed(KeyCode::E) {
+        if input.pressed(KeyCode::Q) || input.pressed(KeyCode::R) {
             let mut y = 0.;
             let mut x = 30.;
             let mut x_vel = 1000.;
@@ -114,7 +197,7 @@ fn shoot_missiles(
                 x_vel = -x_vel;
             }
 
-            for transform in rocket.iter() {
+            for transform in drone.iter() {
                 x += transform.translation.x;
                 y += transform.translation.y;
             }
@@ -150,10 +233,10 @@ fn shoot_missiles(
     }
 }
 
-fn rocket_movement(
+fn drone_movement(
     input: Res<Input<KeyCode>>,
     mut q: QuerySet<(
-        QueryState<(&mut Acceleration, &Transform), With<Rocket>>,
+        QueryState<(&mut Acceleration, &Transform), With<Drone>>,
         QueryState<&mut Transform, With<Camera>>
     )>,
 ) {
@@ -161,7 +244,7 @@ fn rocket_movement(
     let mut thrust = 0;
 
 
-    if input.pressed(KeyCode::W) {
+    if input.pressed(KeyCode::D) {
         thrust += 1;
     }
 
@@ -173,7 +256,7 @@ fn rocket_movement(
         rotation += 1;
     }
 
-    if input.pressed(KeyCode::D) {
+    if input.pressed(KeyCode::H) {
         rotation -= 1;
     }
 
@@ -193,10 +276,10 @@ fn rocket_movement(
     } 
 }
 
-fn spawn_rocket(
+fn spawn_drone(
     mut commands: Commands,
 ) {
-    let rocket = shapes::Rectangle {
+    let drone = shapes::Rectangle {
         extents: Vec2::new(60., 20.),
         origin: shapes::RectangleOrigin::Center
     };
@@ -205,7 +288,7 @@ fn spawn_rocket(
         .spawn()
         .insert_bundle(
             GeometryBuilder::build_as(
-                &rocket,
+                &drone,
                 DrawMode::Outlined {
                     fill_mode: FillMode::color(Color::CYAN),
                     outline_mode: StrokeMode::new(Color::BLACK, 0.),
@@ -220,7 +303,7 @@ fn spawn_rocket(
         })
         .insert(Acceleration::from_linear(Vec3::ZERO))
         .insert(Velocity::from_linear(Vec3::ZERO))
-        .insert(Rocket);
+        .insert(Drone);
 }
 
 fn spawn_floor(
